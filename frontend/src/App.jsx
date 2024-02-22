db764d7bb0a43e2216b89f507a41cfdff8f21b5a
vanessa-pihlaja <vanessa.pihlaja@outlook.com>
2024-02-22 17:05:59 +0200

    Connected to database. Post doesn't work yet.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 const App = (props) => {
-  const { notes } = props
+  const { recipes } = props
   const imageUrl = 'https://viimeistamuruamyoten.com/wp-content/uploads/2024/02/muhammara-munakoisot.jpg';
 
   return (
     <div>
       <h1>MiaMia</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
+      <p>{recipes[0].name}<br/>
+         {recipes[0].content}</p>
+      <p>{recipes[1].name}<br/>
+         {recipes[1].content}</p>
+      <p>{recipes[2].name}<br/>
+         {recipes[2].content}</p>
       <p>
       <a href="https://viimeistamuruamyoten.com/muhammaralla-ja-linsseilla-taytetyt-munakoisot/">Muhammaralla ja linsseillä täytetyt munakoisot</a>
       <img src={imageUrl} alt="First recipe" />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 import App from './App'
 
-const baseUrl = '/api/notes'
+const baseUrl = '/api/recipes'
 
 axios.get(baseUrl).then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+  const recipes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App recipes={recipes} />)
 })
