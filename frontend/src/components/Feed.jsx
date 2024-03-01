524ea21170a3894d704a00122ade65ac914ae90a
vanessa-pihlaja <vanessa.pihlaja@outlook.com>
2024-03-01 15:25:04 +0200

    Updated UI

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,14 +1,9 @@
-import {
-    Link
-  } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import feedService from '../services/feed';
 
-
-
 const Feed = () => {
   const [recipes, setRecipes] = useState([]);
-
 
   useEffect(() => {
     feedService
@@ -18,21 +13,27 @@ const Feed = () => {
       });
   }, []);
 
+  // Utility function to get the first image URL
+  const getFirstImageUrl = (images) => {
+    if (Array.isArray(images) && images.length > 0) {
+      return images[0]; // Return the first image if it's an array with at least one URL
+    }
+    return images; // Return the image if it's not an array or is an empty array
+  };
 
-    return(
-    <div>
+  return (
+    <div className="feed-container">
       {recipes.map(recipe => (
-        <div key={recipe.title}>
+        <div className="recipe-card" key={recipe.title}>
           <h2>
             <Link to={`/${recipe.title}`}>{recipe.title}</Link>
           </h2>
-          <p>
-          </p>
-          <img src={recipe.images} alt={recipe.title} />
+          <img className="recipe-image" src={getFirstImageUrl(recipe.images)} alt={recipe.title} />
+          {/* You can add more recipe details here */}
         </div>
       ))}
     </div>
-    )
+  );
 }
 
-export default Feed
+export default Feed;
